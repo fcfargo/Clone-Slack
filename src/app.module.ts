60b5fc02a78e60c036813068ef8b3a2b36cff43b
fcfargo <fcfargo90@gmail.com>
2022-08-13 02:21:49 +0900

    controller 생성 && swagger 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { DmsModule } from './dms/dms.module';
+import { ChannelsModule } from './channels/channels.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DmsModule, ChannelsModule, WorkspacesModule],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [AppService, ConfigService, UsersService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AppService {
+  constructor(private userService: UsersService) {}
   getHello(): string {
-    return 'Hello World!';
+    this.userService.getUser();
+    return process.env.SECRET;
   }
 }
