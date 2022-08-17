7e5137a5dc2b4d7b8f31fd54a4da1dfc291d714b
fcfargo <74285463+fcfargo@users.noreply.github.com>
2022-08-17 20:43:08 +0900

    feat: type orm settings (#6)
    * typeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,30 @@ import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { UsersService } from './users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DmsModule, ChannelsModule, WorkspacesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      keepConnectionAlive: true,
+      migrations: [__dirname + '/migrations/*.ts'],
+      charset: 'utf8mb4',
+      synchronize: false,
+      logging: true,
+    }),
+    UsersModule,
+    DmsModule,
+    ChannelsModule,
+    WorkspacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigService, UsersService],
 })
